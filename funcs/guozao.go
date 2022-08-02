a3733b728b6c9a6c9f7e29136ae2aaae2920c06d
jizizr <jizizr1234@gmail.com>
2022-08-02 13:44:16 +0800

    新功能

---
+++ funcs/guozao.go
@@ -0,0 +1,36 @@
+package funcs
+
+import (
+	"bot/botTool"
+	"fmt"
+	"regexp"
+	"strings"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+var guozaoMatch = regexp.MustCompile(`[^a-zA-Z0-9_]+`)
+
+func Guozao(update *tgbotapi.Update) {
+	arr := strings.Split(update.Message.Text, " ")
+	if !guozaoMatch.MatchString(arr[0][1:]) {
+		return
+	}
+	var str string
+	var player1, player2 string
+
+	player1 = getAt(update)
+	if update.Message.ReplyToMessage != nil {
+		player2 = getReplyAt(update)
+	} else {
+		player2 = fmt.Sprintf("[自己](tg://user?id=%d)", update.Message.From.ID)
+	}
+
+	if len(arr) == 1 {
+		str = fmt.Sprintf("%s %s了 %s！", player1, arr[0][1:], player2)
+	} else {
+		str = fmt.Sprintf("%s %s %s %s！", player1, arr[0][1:], player2, strings.Join(arr[1:], " "))
+	}
+	str = strings.Replace(strings.Replace(str, "$from", player1, -1), "$to", player2, -1)
+	botTool.SendMessage(update, &str, true, "Markdown")
+}
